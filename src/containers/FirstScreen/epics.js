c1dfea0be871ae371f6e9fd429bf6ba9eee6d8d2
rishithareddy <rishithareddynallakalva@gmail.com>
2024-03-30 04:57:14 +0530

    adding styling

--- src/containers/FirstScreen/epics.js
+++ src/containers/FirstScreen/epics.js
@@ -5,46 +5,45 @@ import { of } from 'rxjs';
 import { fetchPhotoSuccess, fetchPhotoError } from './actions';
 import { FETCH_PHOTO } from './constants';
 
+// Define the base URL for development and production
+const BASE_URL_DEV = 'http://localhost:3001';
+const BASE_URL_PROD = 'https://example.com/api';
+
 export const fetchPhotosEpic = action$ => action$.pipe(
   ofType(FETCH_PHOTO),
   switchMap(action => {
-    // Default URL
-    let url = `http://localhost:3001/externalapi/photos?`;
+    // Determine the base URL based on the environment
+    const baseUrl = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
 
     // Extract pagination and filtering parameters from action payload
-    const { limit = 25, offset = 0, title = '', userEmail = '',albumTitle='' } = action.payload;
+    const { limit = 25, offset = 0, title = '', userEmail = '', albumTitle = '' } = action.payload;
+
+    // Construct the complete URL with pagination and filtering parameters
+    let url = `${baseUrl}/externalapi/photos?`;
 
     // Append filters to the URL if they exist
-    if(title && albumTitle && userEmail){
-      url+=`title=${title}&albumTitle=${albumTitle}&userEmail=${userEmail}&`
-    }
-    else if(title && albumTitle){
-      url+=`title=${title}&albumTitle=${albumTitle}&`
-    }
-    else if(title && userEmail){
-      url+=`title=${title}&userEmail=${userEmail}&`
-    }
-    else if(albumTitle && userEmail){
-      url+=`albumTitle=${albumTitle}&userEmail=${userEmail}&`
-    }
-    else if (title) {
+    if (title && albumTitle && userEmail) {
+      url += `title=${title}&albumTitle=${albumTitle}&userEmail=${userEmail}&`;
+    } else if (title && albumTitle) {
+      url += `title=${title}&albumTitle=${albumTitle}&`;
+    } else if (title && userEmail) {
+      url += `title=${title}&userEmail=${userEmail}&`;
+    } else if (albumTitle && userEmail) {
+      url += `albumTitle=${albumTitle}&userEmail=${userEmail}&`;
+    } else if (title) {
       url += `title=${title}&`;
-    }
-    else if (userEmail) {
+    } else if (userEmail) {
       url += `userEmail=${userEmail}&`;
-    }
-    else if(albumTitle){
-      url+=`albumTitle=${albumTitle}&`
+    } else if (albumTitle) {
+      url += `albumTitle=${albumTitle}&`;
     }
 
     // Append pagination parameters to the URL
     url += `limit=${limit}&offset=${offset}`;
 
+    // Make the AJAX request using the constructed URL
     return ajax.getJSON(url).pipe(
-      map(response => {
-        console.log('I am epics response', response); // Log the response
-        return fetchPhotoSuccess(response);
-      }),
+      map(response => fetchPhotoSuccess(response)),
       catchError(error => of(fetchPhotoError(error)))
     );
   })
--- src/containers/FirstScreen/index.js
+++ src/containers/FirstScreen/index.js
@@ -10,26 +10,38 @@ const PhotoComponent = () => {
   const [tempFilters, setTempFilters] = useState({ title: '', albumTitle: '', userEmail: '' });
 
   const { title, albumTitle, userEmail } = filters;
-  const { limit, offset,currentPage } = pagination;
+  const { limit, offset, currentPage } = pagination;
 
   useEffect(() => {
     dispatch(fetchPhoto({ ...filters, limit, offset }));
   }, [dispatch, filters, limit, offset]);
 
-
   const photos = useSelector(state => state.photo);
-
+const loading=useSelector(state=>state.loading)
   const handleFilterChange = (event, filterKey) => {
     setTempFilters({ ...tempFilters, [filterKey]: event.target.value });
+  };
+
+  const handleLimitChange = (event) => {
+    const newLimit = parseInt(event.target.value);
+    setPagination(prevPagination => ({
+      ...prevPagination,
+      limit: newLimit,
+      offset: 0, // Reset offset when changing limit
+      currentPage: 0 // Reset currentPage when changing limit
+    }));
+    dispatch(fetchPhoto({ ...filters, limit: newLimit, offset: 0 }));
   };
 
   const handleSubmit = () => {
     setFilters(tempFilters); // Update filters with temporary filter data
     dispatch(fetchPhoto({ ...tempFilters, limit, offset }));
   };
+
   const handlePagination = (newOffset) => {
     setPagination({ ...pagination, offset: newOffset, currentPage: Math.ceil((newOffset + 1) / limit) });
   };
+
   useEffect(() => {
     if (photos && photos.total) {
       setPagination({ ...pagination, currentPage: Math.ceil((offset + 1) / limit) });
@@ -37,58 +49,74 @@ const PhotoComponent = () => {
   }, [photos, limit, offset]);
 
   const handleStartPageClick = () => {
-    console.log('i am at start click')
     handlePagination(0);
   };
 
   const handleEndPageClick = () => {
-    const totalPages=Math.ceil(5000/25)
+    const totalPages = Math.ceil(5000 / limit);
     handlePagination((totalPages - 1) * limit);
   };
 
-  return (
-    <div>
-      <div className="header">
-        <h2>Meta Photos</h2>
+ 
+
+return (
+  <div>
+    <div className="header">
+      <h2>Meta Photos</h2>
+    </div>
+    <div className="filter-container">
+      <input type="text" placeholder="Filter by Photo Title" value={tempFilters.title} onChange={(e) => handleFilterChange(e, 'title')} className="input-field" />
+      <input type="text" placeholder="Filter by Album Title" value={tempFilters.albumTitle} onChange={(e) => handleFilterChange(e, 'albumTitle')} className="input-field" />
+      <input type="text" placeholder="Filter by email" value={tempFilters.userEmail} onChange={(e) => handleFilterChange(e, 'userEmail')} className="input-field" />
+      <button onClick={handleSubmit} className="submit-button">Submit</button>
+    </div>
+    <div className="limit-dropdown-container">
+      <div className="limit-dropdown">
+        <label htmlFor="limit">Records Per Page: </label>
+        <select id="limit" value={limit} onChange={handleLimitChange} disabled={photos.photo.length < limit }>
+          <option value={25}>25</option>
+          <option value={50}>50</option>
+          <option value={75}>75</option>
+          <option value={100}>100</option>
+        </select>
       </div>
-      <div className="filter-container">
-        <input type="text" placeholder="Filter by Photo Title" value={tempFilters.title} onChange={(e) => handleFilterChange(e, 'title')} className="input-field" />
-        <input type="text" placeholder="Filter by Album Title" value={tempFilters.albumTitle} onChange={(e) => handleFilterChange(e, 'albumTitle')} className="input-field" />
-        <input type="text" placeholder="Filter by email" value={tempFilters.userEmail} onChange={(e) => handleFilterChange(e, 'userEmail')} className="input-field" />
-        <button onClick={handleSubmit} className="submit-button">Submit</button>
-      </div>
-      {photos && photos.photo && photos.photo.length > 0 ? (
-        <div className="photo-gallery">
-          <div className="gallery-container">
-            {photos.photo.map((photoItem, index) => (
-              <div key={photoItem.id} className="gallery-item">
-                <img src={photoItem.thumbnailUrl} alt={photoItem.title} />
-                <div className="item-content">
-                  <p>
-                    <strong>Title:</strong> {photoItem.title} {photoItem.id}<br />
-                    <strong>Album Title:</strong> {photoItem.album.title}<br />
-                    <strong>UserEmail:</strong> {photoItem.album.user.email}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      ) : (
-        <div className="no-data-message">
-          <p>The filtered data does not exist.</p>
-        </div>
-      )}
       <div className="pagination-buttons">
-        <button onClick={() => handleStartPageClick()} disabled={offset === 0 || !photos || !photos.photo || photos.photo.length === 0}>start page</button>
+        <button onClick={handleStartPageClick} disabled={offset === 0 || !photos || !photos.photo || photos.photo.length === 0}>Start Page</button>
         <button onClick={() => handlePagination(offset - limit)} disabled={offset === 0 || !photos || !photos.photo || photos.photo.length === 0}>Previous</button>
-        <button onClick={() => handlePagination(offset + limit)} disabled={offset + limit >= 5000 || !photos || !photos.photo || photos.photo.length === 0}>Next</button>
-        <button onClick={() => handleEndPageClick()} disabled={offset + limit >= 5000 || !photos || !photos.photo || photos.photo.length === 0}>End page</button>
+        <button onClick={() => handlePagination(offset + limit)} disabled={offset + limit >= 5000 ||  photos.photo.length < limit|| !photos || !photos.photo}>Next</button>
+        <button onClick={handleEndPageClick} disabled={offset + limit >= 5000 || !photos ||photos.photo.length < limit || !photos.photo || photos.photo.length === 0}>End Page</button>
       </div>
     </div>
-  );
-  
-  
+    {/* Display the gallery only if photos are available and data fetching is completed */}
+    {(photos && photos.photo && photos.photo.length > 0) && (
+      <div className="photo-gallery">
+        <div className="gallery-container">
+          {photos.photo.map((photoItem, index) => (
+            <div key={photoItem.id} className="gallery-item">
+              <img src={photoItem.thumbnailUrl} alt={photoItem.title} />
+              <div className="item-content">
+                <p>
+                  <strong>Title:</strong> {photoItem.title} {photoItem.id}<br />
+                  <strong>Album Title:</strong> {photoItem.album.title}<br />
+                  <strong>UserEmail:</strong> {photoItem.album.user.email}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )}
+
+{(!loading && Object.values(tempFilters).some(value => value !== '') && photos && photos.photo && photos.photo.length === 0) && (
+  <div className="no-data-message">
+    <p>The filtered data does not exist.</p>
+  </div>
+)}
+
+  </div>
+);
+
+
 };
 
 export default PhotoComponent;
